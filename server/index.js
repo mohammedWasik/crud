dccb2aeca737db1edd7955171d8a2dee1f50d6c8
Mohammed Wasik <50347228+mohammedWasik@users.noreply.github.com>
2023-05-20 10:31:34 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,10 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
   );
   next();
 });
