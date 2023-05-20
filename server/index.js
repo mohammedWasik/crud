6d5390e62fa7730eed94a121f760e6887ee477e3
Mohammed Wasik <50347228+mohammedWasik@users.noreply.github.com>
2023-05-20 10:18:01 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes/routes.js";
 
-const app = express();
 dotenv.config();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 app.get("/", async (req, res) => {
